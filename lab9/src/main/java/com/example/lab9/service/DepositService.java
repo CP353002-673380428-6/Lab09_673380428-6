323189ab538b97036d40bdd232c8fcd21abb3763
ms584 <sitthichok.m@kkumail.com>
2026-09-18 16:38:08 +0700

    normal code

--- lab9/src/main/java/com/example/lab9/service/DepositService.java
+++ lab9/src/main/java/com/example/lab9/service/DepositService.java
@@ -18,7 +18,7 @@ public class DepositService {
         this.depositRepository = depositRepository;
     }
 
-    //@Transactional
+    @Transactional
     public void deposit(Long accountId, Double amount) {
         // 1. ค้นหา Account จาก accountId
         Account account = accountRepository.findById(accountId)
@@ -33,6 +33,6 @@ public class DepositService {
         depositRepository.save(transaction);
 
         // โยน Error 
-        throw new RuntimeException("Test Rollback");
+        //throw new RuntimeException("Test Rollback");
     }
 }
