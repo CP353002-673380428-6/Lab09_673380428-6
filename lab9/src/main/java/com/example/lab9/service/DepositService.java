447791396442849a3b499766b66fb61a77706dda
ms584 <sitthichok.m@kkumail.com>
2026-09-18 16:37:04 +0700

    comment Transactional

--- lab9/src/main/java/com/example/lab9/service/DepositService.java
+++ lab9/src/main/java/com/example/lab9/service/DepositService.java
@@ -18,7 +18,7 @@ public class DepositService {
         this.depositRepository = depositRepository;
     }
 
-    @Transactional
+    //@Transactional
     public void deposit(Long accountId, Double amount) {
         // 1. ค้นหา Account จาก accountId
         Account account = accountRepository.findById(accountId)
